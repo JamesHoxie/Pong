a1cdca63fbc99e13924f896fd1e3196cb599cc10
JamesHoxie <48693420+JamesHoxie@users.noreply.github.com>
2020-05-24 19:48:55 -0400

    Remove setup file

--- setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("pong.py")]
-
-cx_Freeze.setup(
-	name="Python Pong",
-	options={"build.exe": {"packages": ["pygame"]}},
-	executables = executables
-	)
